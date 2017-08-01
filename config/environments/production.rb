30908a2d8951380d0a0943cb63477b2f8068b45e
66master <takya_ohno@24revive.com>
2017-08-02 07:35:55 +0900

    Add s3 config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,16 @@ Rails.application.configure do
 
   Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin; C:\Program Files\ImageMagick-7.0.6-Q16'
   Paperclip.options[:swallow_stderr] = false
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_protocol: :https,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
+
 end
