7ef7b8d9c5f5280c06e8d53586792c2a51b017c7
66master <takya_ohno@24revive.com>
2017-08-03 06:55:16 +0900

    Edit development aws config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,4 +56,15 @@ Rails.application.configure do
   
   #Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin; C:\Program Files\ImageMagick-7.0.6-Q16'
 
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_protocol: :https,
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      s3_region: ENV['AWS_REGION'],
+    }
+  }
+
   end
